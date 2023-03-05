9f0567e96f30cb668d78a88ca235e3fe788f2960
Serpentarius13 <serpentarium13@mail.ru>
2023-03-05 18:05:09 +0300

    normalize

---
+++ src/components/Buttons/BaseEverywhereButton.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script setup lang="ts">
+
+const props = defineProps(['color', 'text'])
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {},
+    colors: {
+      'main-green': '#14A800',
+      'main-blue': '#0569FF',
+    },
   },
   plugins: [],
 };
