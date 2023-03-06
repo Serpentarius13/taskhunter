594f668654a498f21d5132310c83f9698a82a540
Serpentarius13 <serpentarium13@mail.ru>
2023-03-06 21:17:15 +0300

    form

---
+++ src/features/composables/useToastedForm.ts
@@ -0,0 +1,31 @@
+import { useToast } from "vue-toastification";
+import { useField, useForm } from "vee-validate";
+import { toFormValidator } from "@vee-validate/zod";
+import { z } from "zod";
+
+export default (schema: z.ZodObject<unkown>, initialValues: Record<string, unkown>) => {
+  const toast = useToast();
+  const zodSchema = toFormValidator(schema);
+
+  const { validate: runValidator } = useForm({
+    validationSchema: zodSchema,
+    initialValues: initialValues,
+    validateOnMount: false,
+  });
+
+  const validate = async () => {
+    const { valid, errors } = await runValidator();
+    if (!valid) {
+      toast.clear();
+      const errArr = Object.values(errors);
+      for (const err of errArr) {
+        toast.error(err);
+      }
+      return false;
+    } else {
+      return true;
+    }
+  };
+
+  return validate;
+};
